941596555bf5c5ade3f02722f0564ff84c97daf3
Abdurrahman Javat <a.javat70@gmail.com>
2025-04-13 16:02:40 +0300

    added ls_files function

--- git.py
+++ git.py
@@ -122,3 +122,11 @@ def read_index():
     assert len(entries) == num_entries
     return entries
         
+def ls_files(details=False):
+    for entry in read_index():
+        if details:
+            stage = (entry.flags >> 12) & 3
+            print('{:6o} {} {:}\t{}'.format(
+                    entry.mode, entry.sha1.hex(), stage, entry.path))
+        else:
+            print(entry.path)
