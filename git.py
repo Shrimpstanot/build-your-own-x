87d9a28d8291293c2c5de24fe2f02c20703369d8
Abdurrahman Javat <a.javat70@gmail.com>
2025-04-13 10:01:28 +0300

    added  function

--- git.py
+++ git.py
@@ -90,3 +90,35 @@ def cat_file(mode, sha1_prefix):
     else:
         raise ValueError('unexpected mode {!r}'.format(mode))
     
+IndexEntry = collections.namedtuple('IndexEntry', [
+    'ctime_s', 'ctime_n', 'mtime_s', 'mtime_n', 'dev', 'ino', 'mode',
+    'uid', 'gid', 'size', 'sha1', 'flags', 'path',
+])
+
+def read_index():
+    try:
+        data = read_file(os.path.join(".git", "index"))
+    except FileNotFoundError:
+        return []
+    digest = hashlib.sha1(data[:-20]).digest()
+    assert digest == data[-20:], "invalid index checksum"
+    signature, version, num_entries = struct.unpack('!4sLL', data[:12])
+    assert signature == b'DIRC', \
+            "invalid index signature {}".format(signature)
+    assert version == 2, "unknown index version {}".format(version)
+    entry_data = data[12:-20]
+    entries = []
+    i = 0
+    while i + 62 < len(entry_data):
+        fields_end = i + 62
+        fields = struct.unpack('!LLLLLLLLLL20sH',
+                               entry_data[i:fields_end])
+        path_end = entry_data.index(b'\x00', fields_end)
+        path = entry_data[fields_end:path_end]
+        entry = IndexEntry(*(fields + (path.decode(),)))
+        entries.append(entry)
+        entry_len = ((62 + len(path) + 8) // 8) * 8
+        i += entry_len
+    assert len(entries) == num_entries
+    return entries
+        
